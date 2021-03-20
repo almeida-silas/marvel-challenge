842f7589c573a46bfb4f5f0cbebbdb54b4a579ff
Silas Almeida <silasadssn@gmail.com>
2021-03-20 06:18:09 -0300

    add: new api config params

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,15 @@
+import { BASE_URL, PRIVATE_KEY, PUBLIC_KEY } from '@env';
+
+import crypto from 'crypto-js';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: BASE_URL,
+
   params: {
-    ts: '',
-    apikey: '',
-    hash: '',
+    ts: `${new Date()}`,
+    apikey: PUBLIC_KEY,
+    hash: crypto.MD5(new Date() + PRIVATE_KEY + PUBLIC_KEY).toString(),
   },
 });
 
