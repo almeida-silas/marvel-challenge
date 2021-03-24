588f5a3807d3bf25ffff9d8c071d31a0eb58112d
Silas Almeida <silasadssn@gmail.com>
2021-03-23 22:56:23 -0300

    add: new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from '../pages/Home';
-import Details from '../pages/Details';
+import Characters from '../pages/Characters';
+import Comics from '../pages/Comics';
+import Series from '../pages/Series';
+import Stories from '../pages/Stories';
+import Events from '../pages/Creators';
+import Creators from '../pages/Creators';
+import DetailsComics from '../pages/Comics/DetailsComics';
+import DetailsSeries from '../pages/Series/DetailsSeries';
+import DetailsEvents from '../pages/Events/DetailsEvents';
+import DetailsStories from '../pages/Stories/DetailsStories';
+import DetailsCharacter from '../pages/Characters/DetailsCharacter';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +21,22 @@ const AppNavigator: React.FC = () => {
     <Stack.Navigator
       initialRouteName="Login"
       screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Details" component={Details} />
+      <Stack.Screen name="Characters" component={Characters} />
+      <Stack.Screen name="DetailsCharacter" component={DetailsCharacter} />
+
+      <Stack.Screen name="Comics" component={Comics} />
+      <Stack.Screen name="DetailsComics" component={DetailsComics} />
+
+      <Stack.Screen name="Series" component={Series} />
+      <Stack.Screen name="DetailsSeries" component={DetailsSeries} />
+
+      <Stack.Screen name="Stories" component={Stories} />
+      <Stack.Screen name="DetailsStories" component={DetailsStories} />
+
+      <Stack.Screen name="Events" component={Events} />
+      <Stack.Screen name="DetailsEvents" component={DetailsEvents} />
+
+      <Stack.Screen name="Creators" component={Creators} />
     </Stack.Navigator>
   );
 };
